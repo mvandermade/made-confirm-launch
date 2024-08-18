c048b1302e22d4e3db9b1868c22020b9ba8bc680
martijnvandermade <33425497+mvandermade@users.noreply.github.com>
2024-08-18 21:05:45 +0200

    Versioning in one place

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -3,9 +3,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 
-const val APP_NAME = "Made Confirm Launch"
-const val APP_VERSION = "1.0.3"
-const val APP_TITLE = "$APP_NAME $APP_VERSION"
+const val APP_TITLE = "Made Confirm Launch"
 
 fun main(args: Array<String>) {
     val cmdArguments = fetchArguments(args)
